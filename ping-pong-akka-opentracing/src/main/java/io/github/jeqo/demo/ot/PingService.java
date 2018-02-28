0cfec568e509c0db7d8d6473944f25a543dab777
Jorge Quilcate Otoya <quilcate.jorge@gmail.com>
2018-02-28 13:30:17 +0100

    refactoring

--- ping-pong-akka-opentracing/src/main/java/io/github/jeqo/demo/ot/PingService.java
+++ ping-pong-akka-opentracing/src/main/java/io/github/jeqo/demo/ot/PingService.java
@@ -8,7 +8,6 @@ import io.opentracing.Scope;
 import io.opentracing.SpanContext;
 import io.opentracing.Tracer;
 import io.opentracing.propagation.Format;
-import io.opentracing.propagation.TextMap;
 import io.opentracing.propagation.TextMapInjectAdapter;
 
 import java.util.HashMap;
@@ -21,15 +20,15 @@ import java.util.Random;
 public class PingService extends AbstractActor {
   private final LoggingAdapter log = Logging.getLogger(getContext().getSystem(), this);
 
-  private final ActorRef pongActor;
+  private final ActorRef pongService;
 
   private final Random random = new Random();
 
   //// (1) Tracer ////
   private final Tracer tracer = TracerBuilder.build("ping-service");
 
-  public PingService(ActorRef pongActor) {
-    this.pongActor = pongActor;
+  public PingService(ActorRef pongService) {
+    this.pongService = pongService;
   }
 
   @Override
@@ -44,6 +43,7 @@ public class PingService extends AbstractActor {
                 scope.span().log("ping received"); //// (3) logs in-context
                 log.info("ping");
                 Thread.sleep(random.nextInt(1000));
+
                 scope.span() //// (4) Tags to add span-scoped metadata
                     .setTag("userId", message.getUserId());
 
@@ -56,7 +56,7 @@ public class PingService extends AbstractActor {
                     spanContext,
                     Format.Builtin.TEXT_MAP,
                     new TextMapInjectAdapter(context));
-                pongActor.tell(new PongMessage(context), pongActor);
+                pongService.tell(new PongMessage(context), pongService);
               }
             })
             .build();
